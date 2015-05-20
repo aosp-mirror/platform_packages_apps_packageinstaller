93568c580d730911a6e2734e79fbe6dc27c1bca1
Jason Monk <jmonk@google.com>
2015-05-20 16:43:42 -0400

    Add permission summaries back
     - Add summaries to manage permissions screen
       - Add some shortcuts to skip loading unneeded labels/icons
         for summary info
     - Add way for Settings to broadcast request for info about
       granted permissions.
    
    Bug: 21078474
    Change-Id: I28582129971385203f4e4316addf73e07ee53940

--- src/com/android/packageinstaller/permission/utils/Utils.java
+++ src/com/android/packageinstaller/permission/utils/Utils.java
@@ -25,8 +25,25 @@ import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.util.TypedValue;
 
+import com.android.packageinstaller.permission.model.AppPermissionGroup;
+import com.android.packageinstaller.permission.model.PermissionApps.PermissionApp;
+
 public class Utils {
+
     private static final String LOG_TAG = "Utils";
+
+    public static final String OS_PKG = "android";
+
+    public static final String[] MODERN_PERMISSION_GROUPS = {
+            Manifest.permission_group.CALENDAR,
+            Manifest.permission_group.CAMERA,
+            Manifest.permission_group.CONTACTS,
+            Manifest.permission_group.LOCATION,
+            Manifest.permission_group.SENSORS,
+            Manifest.permission_group.SMS,
+            Manifest.permission_group.PHONE,
+            Manifest.permission_group.MICROPHONE,
+    };
 
     private Utils() {
         /* do nothing - hide constructor */
@@ -42,22 +59,51 @@ public class Utils {
     }
 
     public static boolean isModernPermissionGroup(String name) {
-        switch (name) {
-            case Manifest.permission_group.CALENDAR:
-            case Manifest.permission_group.CAMERA:
-            case Manifest.permission_group.CONTACTS:
-            case Manifest.permission_group.LOCATION:
-            case Manifest.permission_group.SENSORS:
-            case Manifest.permission_group.SMS:
-            case Manifest.permission_group.PHONE:
-            case Manifest.permission_group.MICROPHONE: {
+        for (String modernGroup : MODERN_PERMISSION_GROUPS) {
+            if (modernGroup.equals(name)) {
                 return true;
             }
-
-            default: {
-                return false;
-            }
         }
+        return false;
+    }
+
+    public static boolean shouldShowPermission(AppPermissionGroup group, boolean showLegacy) {
+        // We currently will not show permissions fixed by the system.
+        // which is what the system does for system components.
+        if (group.isSystemFixed()) {
+            return false;
+        }
+
+        // Yes this is possible. We have leftover permissions that
+        // are not in the final groups and we want to get rid of,
+        // therefore we do not have app ops for legacy support.
+        if (!group.hasRuntimePermission() && !group.hasAppOpPermission()) {
+            return false;
+        }
+
+        final boolean isPlatformPermission = group.getDeclaringPackage().equals(OS_PKG);
+        // Show legacy permissions only if the user chose that.
+        if (isPlatformPermission && !showLegacy
+                && !Utils.isModernPermissionGroup(group.getName())) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean shouldShowPermission(PermissionApp app) {
+        // We currently will not show permissions fixed by the system
+        // which is what the system does for system components.
+        if (app.isSystemFixed()) {
+            return false;
+        }
+
+        // Yes this is possible. We have leftover permissions that
+        // are not in the final groups and we want to get rid of,
+        // therefore we do not have app ops for legacy support.
+        if (!app.hasRuntimePermissions() && !app.hasAppOpPermissions()) {
+            return false;
+        }
+        return true;
     }
 
     public static Drawable applyTint(Context context, Drawable icon, int attr) {
