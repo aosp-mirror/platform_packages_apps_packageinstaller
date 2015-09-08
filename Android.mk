5b26c2e64ceeaa1e71436b36a28f53100e358ed8
Griff Hazen <griff@google.com>
2015-09-08 22:59:10 +0000

    Revert "Move ClockworkPackageInstaller functionality here"
    Breaking ub-wear-dinar/ub-wear-master builds
    
    This reverts commit aa4368cafc3c92b33a99c3fbe0e6af08d7e092e4.
    
    Change-Id: Ib22189891e9cd9eef61cdaa5ac329d05ab9783e8

--- Android.mk
+++ Android.mk
@@ -15,7 +15,6 @@ LOCAL_STATIC_JAVA_LIBRARIES += \
     android-support-v14-preference \
     android-support-v17-preference-leanback \
     android-support-v17-leanback \
-    xz-java
 
 LOCAL_RESOURCE_DIR := \
     frameworks/support/v17/leanback/res \
