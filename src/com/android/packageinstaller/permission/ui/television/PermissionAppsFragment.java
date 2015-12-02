dede951ec7d3d7d15b6d82d87cc4407ab96e6357
Svet Ganov <svetoslavganov@google.com>
2015-12-01 21:08:00 -0800

    Add some things missing in the previous CL.
    Change-Id: I17ae37d84c0d78bcfcaa848efb17f46c7f7c915a

--- src/com/android/packageinstaller/permission/ui/television/PermissionAppsFragment.java
+++ src/com/android/packageinstaller/permission/ui/television/PermissionAppsFragment.java
@@ -46,6 +46,7 @@ import com.android.packageinstaller.permission.model.PermissionApps;
 import com.android.packageinstaller.permission.model.PermissionApps.Callback;
 import com.android.packageinstaller.permission.model.PermissionApps.PermissionApp;
 import com.android.packageinstaller.permission.ui.OverlayTouchActivity;
+import com.android.packageinstaller.permission.ui.ReviewPermissionsActivity;
 import com.android.packageinstaller.permission.utils.LocationUtils;
 import com.android.packageinstaller.permission.utils.SafetyNetLogger;
 import com.android.packageinstaller.permission.utils.Utils;
