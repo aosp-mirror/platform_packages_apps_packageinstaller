2a55949c2d630c4a96b83c3fb4b8d65bb762c7dd
Amith Yamasani <yamasani@google.com>
2016-03-09 13:36:52 -0800

    Tweak strings when uninstalling updates
    Inform about data deletion and also about other users.
    
    Bug: 26694521
    Change-Id: Ib1ab8ca58e111cfbad6895f296e567e289ce34d8

--- src/com/android/packageinstaller/UninstallerActivity.java
+++ src/com/android/packageinstaller/UninstallerActivity.java
@@ -74,11 +74,15 @@ public class UninstallerActivity extends Activity {
 
             final boolean isUpdate =
                     ((dialogInfo.appInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0);
+            UserManager userManager = UserManager.get(getActivity());
             if (isUpdate) {
-                messageBuilder.append(getString(R.string.uninstall_update_text));
+                if (isSingleUser(userManager)) {
+                    messageBuilder.append(getString(R.string.uninstall_update_text));
+                } else {
+                    messageBuilder.append(getString(R.string.uninstall_update_text_multiuser));
+                }
             } else {
-                UserManager userManager = UserManager.get(getActivity());
-                if (dialogInfo.allUsers && userManager.getUserCount() >= 2) {
+                if (dialogInfo.allUsers && !isSingleUser(userManager)) {
                     messageBuilder.append(getString(R.string.uninstall_application_text_all_users));
                 } else if (!dialogInfo.user.equals(android.os.Process.myUserHandle())) {
                     UserInfo userInfo = userManager.getUserInfo(dialogInfo.user.getIdentifier());
@@ -112,6 +116,16 @@ public class UninstallerActivity extends Activity {
             if (isAdded()) {
                 getActivity().finish();
             }
+        }
+
+        /**
+         * Returns whether there is only one user on this device, not including
+         * the system-only user.
+         */
+        private boolean isSingleUser(UserManager userManager) {
+            final int userCount = userManager.getUserCount();
+            return userCount == 1
+                    || (UserManager.isSplitSystemUser() && userCount == 2);
         }
     }
 
