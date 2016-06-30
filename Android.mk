97c643608530fae317374b989fe5dc06480673cb
Adam Lesinski <adamlesinski@google.com>
2016-06-29 22:07:36 -0700

    Convert PackageInstaller to build with AAPT2
    Bug:29462255
    Change-Id: Ic804b5cd4e00c48a735adad5c421387122587ba5

--- Android.mk
+++ Android.mk
@@ -1,32 +1,26 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_USE_AAPT2 := true
+
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
     $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES += \
+LOCAL_STATIC_ANDROID_LIBRARIES += \
     android-support-v4 \
+    android-support-annotations \
     android-support-v7-recyclerview \
     android-support-v7-preference \
     android-support-v7-appcompat \
     android-support-v14-preference \
     android-support-v17-preference-leanback \
     android-support-v17-leanback \
+    SettingsLib
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
     xz-java
-
-LOCAL_RESOURCE_DIR := \
-    frameworks/support/v17/leanback/res \
-    frameworks/support/v7/preference/res \
-    frameworks/support/v14/preference/res \
-    frameworks/support/v17/preference-leanback/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/recyclerview/res \
-    $(LOCAL_PATH)/res
-
-LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v17.leanback:android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview
 
 LOCAL_PACKAGE_NAME := PackageInstaller
 LOCAL_CERTIFICATE := platform
@@ -37,7 +31,5 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 # Comment for now unitl all private API dependencies are removed
 # LOCAL_SDK_VERSION := system_current
-
-include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
