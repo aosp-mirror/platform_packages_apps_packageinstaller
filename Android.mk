0c0687d666f6770690916a7807f9df69628f63a9
Philip P. Moltmann <moltmann@google.com>
2016-10-26 09:45:44 -0700

    UI tweaks for package installer
    - separate tablet and phone UI
    - Switch to AAPT1 until APPT2 bugs preventing HW accelleration is fixed.
    - Set elevation for action bar.
    
    Missing: - install_start has to be split into the various variations
             - tablet mode needs to be throughly tested
    
    Test: Ran installation on phone
    Change-Id: Ia1c72ee34e945b6a872be35801ea18a901cbb38f

--- Android.mk
+++ Android.mk
@@ -1,7 +1,18 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_USE_AAPT2 := true
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages android.support.v17.leanback:android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview
+
+LOCAL_RESOURCE_DIR := \
+    frameworks/support/v17/leanback/res \
+    frameworks/support/v7/preference/res \
+    frameworks/support/v14/preference/res \
+    frameworks/support/v17/preference-leanback/res \
+    frameworks/support/v7/appcompat/res \
+    frameworks/support/v7/recyclerview/res \
+    frameworks/base/packages/SettingsLib/res \
+    $(LOCAL_PATH)/res
 
 LOCAL_MODULE_TAGS := optional
 
