7e13c6bd25c7cfb077e732a7458efaf2954f9905
Suprabh Shukla <suprabh@google.com>
2017-07-05 16:31:36 -0700

    DO NOT MERGE Disable overlays while installer is resumed
    The install button can be disabled for other reasons, hence we have to
    store the enabled state independent of the resumed/paused state.
    
    Test: gts-tradefed run gts-dev -m PackageInstallerTapjacking
    
    Bug: 35056974
    Change-Id: I2effa0f5afacfaed217a030550a778e32912cfbb
    (cherry picked from commit 287d1d11a321040948d6c12e48fa303eed3c4859)

--- src/com/android/packageinstaller/PackageInstallerActivity.java
+++ src/com/android/packageinstaller/PackageInstallerActivity.java
@@ -16,7 +16,6 @@
 */
 package com.android.packageinstaller;
 
-import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -52,6 +51,8 @@ import android.widget.Button;
 import android.widget.TabHost;
 import android.widget.TextView;
 
+import com.android.packageinstaller.permission.ui.OverlayTouchActivity;
+
 import java.io.File;
 import java.util.List;
 
@@ -65,7 +66,8 @@ import java.util.List;
  * Based on the user response the package is then installed by launching InstallAppConfirm
  * sub activity. All state transitions are handled in this activity
  */
-public class PackageInstallerActivity extends Activity implements OnCancelListener, OnClickListener {
+public class PackageInstallerActivity extends OverlayTouchActivity implements OnCancelListener,
+        OnClickListener {
     private static final String TAG = "PackageInstaller";
 
     private int mSessionId = -1;
@@ -560,6 +562,25 @@ public class PackageInstallerActivity extends Activity implements OnCancelListen
                     InstallFlowAnalytics.RESULT_BLOCKED_BY_UNKNOWN_SOURCES_SETTING);
         } else {
             initiateInstall();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mOk != null) {
+            mOk.setEnabled(mOkCanInstall);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mOk != null) {
+            // Don't allow the install button to be clicked as there might be overlays
+            mOk.setEnabled(false);
         }
     }
 
