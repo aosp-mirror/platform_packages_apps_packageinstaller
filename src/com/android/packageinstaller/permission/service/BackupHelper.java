d273473b46c89e97e3cabcd125679ab45549b9b3
Philip P. Moltmann <moltmann@google.com>
2019-04-02 15:44:38 -0700

    Remove code resetting the loc perm on upgrade
    Bug: 129340674
    Test: Restored location permissions
    Change-Id: I3cf8546676737d9866abe38f122d0ca02095e768

--- src/com/android/packageinstaller/permission/service/BackupHelper.java
+++ src/com/android/packageinstaller/permission/service/BackupHelper.java
@@ -20,11 +20,9 @@ import static android.content.Context.MODE_PRIVATE;
 import static android.content.pm.PackageManager.FLAG_PERMISSION_POLICY_FIXED;
 import static android.content.pm.PackageManager.FLAG_PERMISSION_SYSTEM_FIXED;
 import static android.content.pm.PackageManager.GET_PERMISSIONS;
-import static android.provider.Settings.Secure.LOCATION_PERMISSIONS_UPGRADE_TO_Q_MODE;
 import static android.util.Xml.newSerializer;
 
 import static com.android.packageinstaller.Constants.DELAYED_RESTORE_PERMISSIONS_FILE;
-import static com.android.packageinstaller.permission.utils.Utils.getGroupOfPlatformPermission;
 
 import static org.xmlpull.v1.XmlPullParser.END_DOCUMENT;
 import static org.xmlpull.v1.XmlPullParser.END_TAG;
@@ -32,7 +30,6 @@ import static org.xmlpull.v1.XmlPullParser.START_TAG;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -40,8 +37,6 @@ import android.os.Build;
 import android.os.UserHandle;
 import android.permission.PermissionManager;
 import android.permission.PermissionManager.SplitPermissionInfo;
-import android.provider.Settings;
-import android.text.TextUtils;
 import android.util.Log;
 import android.util.Xml;
 
@@ -62,7 +57,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -413,15 +407,6 @@ public class BackupHelper {
             if (permName == null) {
                 throw new XmlPullParserException("Found " + TAG_PERMISSION + " without "
                         + ATTR_PERMISSION_NAME);
-            }
-
-            // Clear location permission on upgrade to Q or later
-            String group = getGroupOfPlatformPermission(permName);
-            if (backupPlatformVersion < Build.VERSION_CODES.Q && TextUtils.equals(group,
-                    Manifest.permission_group.LOCATION)
-                    && Settings.Secure.getInt(context.getContentResolver(),
-                    LOCATION_PERMISSIONS_UPGRADE_TO_Q_MODE, 0) == 0) {
-                return Collections.emptyList();
             }
 
             ArrayList<String> expandedPermissions = new ArrayList<>();
