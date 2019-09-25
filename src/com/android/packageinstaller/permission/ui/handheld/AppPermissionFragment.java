e40f080ac7b1a0186cab38473a2a88bdc20c42d2
Joel Galenson <jgalenson@google.com>
2019-09-25 08:50:14 -0700

    Re-add code that somehow got deleted.
    Test: View screen with and without PH enabled for modern and
    non-modern groups.
    
    Change-Id: Ibf92536a44759beaffab6212b247336119e6c054

--- src/com/android/packageinstaller/permission/ui/handheld/AppPermissionFragment.java
+++ src/com/android/packageinstaller/permission/ui/handheld/AppPermissionFragment.java
@@ -173,7 +173,20 @@ public class AppPermissionFragment extends SettingsWithLargeHeader {
         ((TextView) root.requireViewById(R.id.permission_message)).setText(
                 context.getString(R.string.app_permission_header, mGroup.getFullLabel()));
 
-        root.requireViewById(R.id.usage_summary).setVisibility(View.GONE);
+        if (!Utils.isPermissionsHubEnabled()) {
+            root.requireViewById(R.id.usage_summary).setVisibility(View.GONE);
+        } else if (Utils.isModernPermissionGroup(mGroup.getName())) {
+            if (!Utils.shouldShowPermissionUsage(mGroup.getName())) {
+                ((TextView) root.requireViewById(R.id.usage_summary)).setText(
+                        context.getString(R.string.app_permission_footer_not_available));
+            } else {
+                ((TextView) root.requireViewById(R.id.usage_summary)).setText(
+                        getUsageSummary(context, appLabel));
+            }
+        } else {
+            root.requireViewById(R.id.usage_summary).setVisibility(View.GONE);
+        }
+
         long sessionId = getArguments().getLong(EXTRA_SESSION_ID);
 
         TextView footer1Link = root.requireViewById(R.id.footer_link_1);
