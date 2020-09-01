489071add405ac7cae34997cd4a788a3be647014
Nate Myren <ntmyren@google.com>
2020-09-01 12:43:21 -0700

    use ObserveStale for liveData in reviewOngoingUsage
    Bug: 166837151
    Fixes: 167431234
    Test: Open the privacy chip twice quickly. It should load both times
    Change-Id: Id828a89c805b527c2b1457d5469fa6626c07065d
    Merged-In: I2837d48b3e0c985d3023bf75f3876e0e97a0d9ae

--- src/com/android/permissioncontroller/permission/ui/handheld/ReviewOngoingUsageFragment.java
+++ src/com/android/permissioncontroller/permission/ui/handheld/ReviewOngoingUsageFragment.java
@@ -107,7 +107,7 @@ public class ReviewOngoingUsageFragment extends PreferenceFragmentCompat {
         }
         ArrayList<String> appOps = new ArrayList<>(List.of(PHONE_CALL, VIDEO_CALL));
         mOpUsageLiveData = OpUsageLiveData.Companion.get(appOps, numMillis);
-        mOpUsageLiveData.observe(this, new Observer<Map<String, List<OpAccess>>>() {
+        mOpUsageLiveData.observeStale(this, new Observer<Map<String, List<OpAccess>>>() {
             @Override
             public void onChanged(Map<String, List<OpAccess>> opUsage) {
                 if (mOpUsageLiveData.isStale()) {
